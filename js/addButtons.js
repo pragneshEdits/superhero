d1ab1b81b84ec8de94d1638dde259e9fb9724bc8
pragneshEdits <73236844+pragneshEdits@users.noreply.github.com>
2022-12-26 15:03:37 +0530

    Add files via upload

---
+++ js/addButtons.js
@@ -0,0 +1,19 @@
+AFRAME.registerComponent("createbuttons", {
+  init: function() {
+    var button1 = document.createElement("button");
+    button1.innerHTML = "ORDER THOR TOY NOW";
+    button1.setAttribute("id", "order-button");
+    button1.setAttribute("class", "btn btn-danger ml-3 mr-3");
+
+    // 2. Create the Bill button
+    var button2 = document.createElement("button");
+    button2.innerHTML = "ORDER SUMMARY";
+    button2.setAttribute("id", "order-summary-button");
+    button2.setAttribute("class", "btn btn-danger ml-3");
+
+    // 3. Append somewhere
+    var buttonDiv = document.getElementById("button-div");
+    buttonDiv.appendChild(button2);
+    buttonDiv.appendChild(button1);
+  }
+});
